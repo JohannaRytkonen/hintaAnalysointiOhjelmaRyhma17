059c3db6c7135dba01d79a4d61cfff0385f0542a
Playerguy23 <maa1kivi@gmail.com>
2023-12-11 16:01:56 +0200

    Lisatty tiedoston luku metodi.

--- src/hintaAnalysointiOhjelma/FileManager.java
+++ src/hintaAnalysointiOhjelma/FileManager.java
@@ -1,5 +1,30 @@
 package hintaAnalysointiOhjelma;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class FileManager {
-	//TODO: Staattinen metodi, joka lukee tiedoston sille annetusta syötteestä
+	public static ArrayList<String> tiedostonLuku(String tiedostoPolku) {
+		try {
+			ArrayList<String> rivit = new ArrayList<>();
+			
+			FileReader fr = new FileReader(tiedostoPolku);
+			BufferedReader br = new BufferedReader(fr);
+			
+			String rivi;
+			while((rivi = br.readLine()) != null) {
+				rivit.add(rivi);
+			}
+			
+			br.close();
+			fr.close();
+			
+			return rivit;
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+			return null;
+		}
+	}
 }

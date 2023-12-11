1f3e7e06e0c36bf8f433e6dcede94bd7fb2a107d
Playerguy23 <maa1kivi@gmail.com>
2023-12-11 17:00:25 +0200

    Lisatty alku ComputationResult luokkaan.

--- src/hintaAnalysointiOhjelma/ComputationResult.java
+++ src/hintaAnalysointiOhjelma/ComputationResult.java
@@ -1,5 +1,81 @@
 package hintaAnalysointiOhjelma;
 
+import java.util.ArrayList;
+
 public class ComputationResult {
-	//TODO: luokka, joka laskee kokonaisuuden. Esim. keskiarvo
+	// TODO: luokka, joka laskee kokonaisuuden. Esim. keskiarvo
+	private double kesikarvoSulkemisHinta;
+	private double kesikarvoAvausHinta;
+	private double kesikarvoKorkeinHinta;
+	private double kesikarvoMatalinHinta;
+
+	private double keskihajontaSulkemisHinta;
+	private double keskihajontaAvausHinta;
+	private double keskihajontaKorkeinHinta;
+	private double keskihajontaMatalinHinta;
+
+	public double getKesikarvoSulkemisHinta() {
+		return kesikarvoSulkemisHinta;
+	}
+
+	public void setKesikarvoSulkemisHinta(ArrayList<PriceTick> paivaHintaRivit) {
+		// TODO: 
+	}
+
+	public double getKesikarvoAvausHinta() {
+		return kesikarvoAvausHinta;
+	}
+
+	public void setKesikarvoAvausHinta(ArrayList<PriceTick> paivaHintaRivit) {
+		// TODO: 
+	}
+
+	public double getKesikarvoKorkeinHinta() {
+		return kesikarvoKorkeinHinta;
+	}
+
+	public void setKesikarvoKorkeinHinta(ArrayList<PriceTick> paivaHintaRivit) {
+		// TODO: 
+	}
+
+	public double getKesikarvoMatalinHinta() {
+		return kesikarvoMatalinHinta;
+	}
+
+	public void setKesikarvoMatalinHinta(ArrayList<PriceTick> paivaHintaRivit) {
+		// TODO: 
+	}
+
+	public double getKeskihajontaSulkemisHinta() {
+		return keskihajontaSulkemisHinta;
+	}
+
+	public void setKeskihajontaSulkemisHinta(ArrayList<PriceTick> paivaHintaRivit) {
+		// TODO: 
+	}
+
+	public double getKeskihajontaAvausHinta() {
+		return keskihajontaAvausHinta;
+	}
+
+	public void setKeskihajontaAvausHinta(ArrayList<PriceTick> paivaHintaRivit) {
+		// TODO: 
+	}
+
+	public double getKeskihajontaKorkeinHinta() {
+		return keskihajontaKorkeinHinta;
+	}
+
+	public void setKeskihajontaKorkeinHinta(ArrayList<PriceTick> paivaHintaRivit) {
+		// TODO: 
+	}
+
+	public double getKeskihajontaMatalinHinta() {
+		return keskihajontaMatalinHinta;
+	}
+
+	public void setKeskihajontaMatalinHinta(ArrayList<PriceTick> paivaHintaRivit) {
+		// TODO: 
+	}
+
 }

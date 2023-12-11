a312c5530447a823dad31d56297a9c81da58a18d
JohannaRytkonen <johanna.mustasaari@gmail.com>
2023-12-11 16:52:32 +0200

    PriceTick luokan luonti

--- src/hintaAnalysointiOhjelma/PriceTick.java
+++ src/hintaAnalysointiOhjelma/PriceTick.java
@@ -2,4 +2,44 @@ package hintaAnalysointiOhjelma;
 
 public class PriceTick {
 	// TODO: Luokka ottaa ylös tarvittavat tiedot laskentaan
+	
+	private double sulkemisHinta;
+	
+	public double getSulkemisHinta() {
+		return sulkemisHinta;
+	}
+	public void setSulkemisHinta(double sulkemisHinta) {
+		this.sulkemisHinta = sulkemisHinta;
+	}
+	
+	
+	private double avausHinta;
+	
+	public double getAvausHinta() {
+		return avausHinta;
+	}
+	public void setAvausHinta(double avausHinta) {
+		this.avausHinta = avausHinta;
+	}
+	
+	
+	
+	private double korkeinHinta;
+	
+	public double getKorkeinHinta() {
+		return korkeinHinta;
+	}
+	public void setKorkeinHinta(double korkeinHinta) {
+		this.korkeinHinta = korkeinHinta;
+	}
+	
+	
+	private double matalinHinta;
+
+	public double getMatalinHinta() {
+		return matalinHinta;
+	}
+	public void setMatalinHinta(double matalinHinta) {
+		this.matalinHinta = matalinHinta;
+	}
 }
